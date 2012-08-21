f01dd0a5f2fdfea72a0e1a42eb7ca990bfe95cf3
David de Boer <david@ddeboer.nl>
2012-08-21 11:42:08 +0200

    Use connection string returned by PHP, to match mailbox name properly

--- src/Ddeboer/Imap/Server.php
+++ src/Ddeboer/Imap/Server.php
@@ -17,11 +17,14 @@ class Server
     public function authenticate($username, $password)
     {
         $resource = @\imap_open($this->server, $username, $password, null, 1, array('DISABLE_AUTHENTICATOR' => 'GSSAPI'));
+        $check = imap_check($resource);
+        $mailbox = $check->Mailbox;
+        $this->connection = substr($mailbox, 0, strpos($mailbox, '}')+1);
 
         // These are necessary to get rid of PHP throwing IMAP errors
         imap_errors();
         imap_alerts();
 
-        return new Connection($resource, $this->server);
+        return new Connection($resource, $this->connection);
     }
 }
