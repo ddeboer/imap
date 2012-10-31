7b232dbfa7abde36cea3868278a0329bc2676b7c
Mantas Varatiejus <var.mantas@gmail.com>
2012-11-01 00:56:14 +0200

    Implemented saveToFile() method

--- src/Ddeboer/Imap/Message/Attachment.php
+++ src/Ddeboer/Imap/Message/Attachment.php
@@ -67,4 +67,38 @@ class Attachment extends Part
 
         return $this->contentType;
     }
+
+    /**
+     * Saves attachment to file
+     *
+     * @param string $filePath
+     *
+     * @throws \Ddeboer\Imap\Exception\Exception
+     */
+    public function saveToFile($filePath)
+    {
+        $handle = @fopen($filePath, 'w');
+
+        if ($handle === false) {
+            throw new \Ddeboer\Imap\Exception\Exception('Failed to open stream');
+        }
+
+        switch ($this->encoding) {
+            case self::ENCODING_BASE64:
+                stream_filter_append($handle, 'convert.base64-decode', STREAM_FILTER_WRITE);
+                break;
+            case self::ENCODING_QUOTED_PRINTABLE:
+                stream_filter_append($handle, 'convert.quoted-printable-decode', STREAM_FILTER_WRITE);
+                break;
+            default:
+                // do nothing
+                break;
+        }
+
+        if (!imap_savebody($this->stream, $handle, $this->messageNumber, $this->partNumber)) {
+            throw new \Ddeboer\Imap\Exception\Exception(imap_last_error());
+        }
+
+        fclose($handle);
+    }
 }
