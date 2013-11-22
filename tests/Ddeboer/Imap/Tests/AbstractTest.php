497039ff3c385a5ba83ca809f75a7939f5c2e81a
David de Boer <david@ddeboer.nl>
2013-11-22 20:44:41 +0100

    Add test message functionality to base test case

--- tests/Ddeboer/Imap/Tests/AbstractTest.php
+++ tests/Ddeboer/Imap/Tests/AbstractTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Ddeboer\Imap\Tests;
 
+use Ddeboer\Imap\Mailbox;
 use Ddeboer\Imap\Server;
 use Ddeboer\Imap\Connection;
 
@@ -14,5 +15,21 @@ abstract class AbstractTest extends \PHPUnit_Framework_TestCase
         $server = new Server('imap.gmail.com');
 
         return $server->authenticate(\getenv('EMAIL_USERNAME'), \getenv('EMAIL_PASSWORD'));
+    }
+
+    protected function createTestMessage(
+        Mailbox $mailbox,
+        $subject = 'Don\t panic!',
+        $from = 'someone@there.com',
+        $to = 'me@here.com',
+        $contents = 'Don\'t forget your towel'
+    ) {
+        $message = "From: $from\r\n"
+            . "To: $to\r\n"
+            . "Subject: $subject\r\n"
+            . "\r\n"
+            . "$contents\r\n";
+
+        $mailbox->addMessage($message);
     }
 }
