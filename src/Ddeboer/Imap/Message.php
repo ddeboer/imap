92a802c24d8a4bb079dc194c91378c5e6bf5d713
Tim Helfensdörfer <tim@visualappeal.de>
2014-06-15 11:56:48 +0200

    Catch error if header could not retrieved

--- src/Ddeboer/Imap/Message.php
+++ src/Ddeboer/Imap/Message.php
@@ -171,8 +171,11 @@ class Message extends Message\Part
             // \imap_header is much faster than \imap_fetchheader
             // \imap_header returns only a subset of all mail headers,
             // but it does include the message flags.
-            $headers = \imap_header($this->stream, $this->messageNumber);
-            $this->headers = new Message\Headers($headers);
+            try {
+                $headers = \imap_header($this->stream, $this->messageNumber);
+            } catch (\Exception $e) {
+                throw new Exception\HeadersNotFoundException($this);
+            }
         }
 
         return $this->headers;
