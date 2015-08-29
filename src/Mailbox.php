eb574cb61e19851c8eb4e69ee04a4d91b7e89d01
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2015-08-29 10:20:50 +0200

    Mark as Countable interface
    Client can use native php count function

--- src/Mailbox.php
+++ src/Mailbox.php
@@ -6,7 +6,7 @@ namespace Ddeboer\Imap;
  * An IMAP mailbox (commonly referred to as a ‘folder’)
  *
  */
-class Mailbox implements \IteratorAggregate
+class Mailbox implements \Countable, \IteratorAggregate
 {
     private $mailbox;
     private $name;
