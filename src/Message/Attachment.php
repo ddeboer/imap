95e8e3e0f887cd0d922f75d163837a06c6ce6d04
Sawered <sawered@gmail.com>
2015-10-05 21:32:27 +0300

    fix attachment filename decoding

--- src/Message/Attachment.php
+++ src/Message/Attachment.php
@@ -14,8 +14,15 @@ class Attachment extends Part
      */
     public function getFilename()
     {
-        return $this->parameters->get('filename')
-            ?: $this->parameters->get('name');
+        $fname =  $this->parameters->get('filename')?: $this->parameters->get('name');
+        //RFCs 2047, 2231 and 5987
+        //http://tools.ietf.org/html/rfc5987
+        $marker = "UTF-8''";
+        if(stripos($fname,$marker) ===0){
+            $fname = substr($fname,strlen($marker));//no mb!
+            $fname = urldecode($fname);
+        }
+        return $fname;
     }
 
     /**
