4239d39ec11fa829c120e7081dcc3e010adc41d6
gbcogivea <support@cogivea.com>
2015-12-03 15:52:49 +0100

    Update Attachment.php

--- src/Message/Attachment.php
+++ src/Message/Attachment.php
@@ -2,6 +2,10 @@
 
 namespace Ddeboer\Imap\Message;
 
+use Ddeboer\Transcoder\Transcoder;
+
+use Exception;
+
 /**
  * An e-mail attachment
  */
@@ -14,8 +18,33 @@ class Attachment extends Part
      */
     public function getFilename()
     {
-        return $this->parameters->get('filename')
-            ?: $this->parameters->get('name');
+        /*return $this->parameters->get('filename')
+            ?: $this->parameters->get('name');*/
+
+        $fname =  $this->parameters->get('filename')?: $this->parameters->get('name');
+        //RFCs 2047, 2231 and 5987
+        //http://tools.ietf.org/html/rfc5987
+
+
+        $matches = [];
+        if(preg_match("@^([^']+)''(.*)@ui",$fname,$matches)){
+
+            $fname = urldecode($matches[2]);
+
+
+            try{
+                $fname = Transcoder::create()->transcode(
+                    $fname,
+                    $matches[1],
+                    "UTF-8//IGNORE"
+                );
+            }catch(Exception $e){
+                $fname = 'filename_wrong_enc';
+            }
+        }
+
+
+        return $fname;
     }
 
     /**
