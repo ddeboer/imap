f512d42f37f8ea50e9f965963d817fec7aa81f2e
Zane Hooper <zanehoop@gmail.com>
2017-01-06 16:44:55 -0500

    Update Message.php

--- src/Message.php
+++ src/Message.php
@@ -428,6 +428,8 @@ class Message extends Message\Part
             $this->messageNumber,
             \FT_UID
         );
+
+        restore_error_handler();
         
         if (!$structure) {
             throw new MessageDoesNotExistException(
@@ -435,8 +437,6 @@ class Message extends Message\Part
                 "imap_fetchstructure returned empty message"
             );
         }
-
-        restore_error_handler();
 
         $this->parseStructure($structure);
     }
