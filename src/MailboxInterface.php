43ef23cfba680d6bff798e4bb1c837cceb79cd07
Filippo Tessarotto <zoeslam@gmail.com>
2017-10-09 17:24:13 +0200

    Add missing interface change for #225 (#233)

--- src/MailboxInterface.php
+++ src/MailboxInterface.php
@@ -56,6 +56,26 @@ interface MailboxInterface extends \Countable, \IteratorAggregate
     public function getStatus(int $flags = null): \stdClass;
 
     /**
+     * Bulk Set Flag for Messages.
+     *
+     * @param string       $flag    \Seen, \Answered, \Flagged, \Deleted, and \Draft
+     * @param array|string $numbers Message numbers
+     *
+     * @return bool
+     */
+    public function setFlag(string $flag, $numbers): bool;
+
+    /**
+     * Bulk Clear Flag for Messages.
+     *
+     * @param string       $flag    \Seen, \Answered, \Flagged, \Deleted, and \Draft
+     * @param array|string $numbers Message numbers
+     *
+     * @return bool
+     */
+    public function clearFlag(string $flag, $numbers): bool;
+
+    /**
      * Get message ids.
      *
      * @param ConditionInterface $search Search expression (optional)
