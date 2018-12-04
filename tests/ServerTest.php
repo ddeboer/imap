e4859435bc839cab05a290ffd2e76360bf248382
Filippo Tessarotto <zoeslam@gmail.com>
2018-12-04 11:04:34 +0100

    Add ability to pass options and retries to imap_open (#382)
    Close #315

--- tests/ServerTest.php
+++ tests/ServerTest.php
@@ -42,4 +42,18 @@ final class ServerTest extends AbstractTest
 
         $this->assertInstanceOf(ConnectionInterface::class, $server->authenticate(\getenv('IMAP_USERNAME'), \getenv('IMAP_PASSWORD')));
     }
+
+    public function test_custom_options()
+    {
+        $server = new Server(\getenv('IMAP_SERVER_NAME'), \getenv('IMAP_SERVER_PORT'), self::IMAP_FLAGS, [], \OP_HALFOPEN);
+
+        $connection = $server->authenticate(\getenv('IMAP_USERNAME'), \getenv('IMAP_PASSWORD'));
+
+        $check = \imap_check($connection->getResource()->getStream());
+        $mailbox = \strtolower($check->Mailbox);
+
+        static::assertContains(\getenv('IMAP_USERNAME'), $mailbox);
+        static::assertNotContains('inbox', $mailbox);
+        static::assertContains('no_mailbox', $mailbox);
+    }
 }
