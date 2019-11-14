5b58774101b26bc2ade2e0273886e79db1a08c75
Filippo Tessarotto <zoeslam@gmail.com>
2019-11-14 15:55:03 +0100

    CS Fix

--- src/ConnectionInterface.php
+++ src/ConnectionInterface.php
@@ -11,31 +11,21 @@ interface ConnectionInterface extends \Countable
 {
     /**
      * Get IMAP resource.
-     *
-     * @return ImapResourceInterface
      */
     public function getResource(): ImapResourceInterface;
 
     /**
      * Delete all messages marked for deletion.
-     *
-     * @return bool
      */
     public function expunge(): bool;
 
     /**
      * Close connection.
-     *
-     * @param int $flag
-     *
-     * @return bool
      */
     public function close(int $flag = 0): bool;
 
     /**
      * Check if the connection is still active.
-     *
-     * @return bool
      */
     public function ping(): bool;
 
@@ -50,8 +40,6 @@ interface ConnectionInterface extends \Countable
      * Check that a mailbox with the given name exists.
      *
      * @param string $name Mailbox name
-     *
-     * @return bool
      */
     public function hasMailbox(string $name): bool;
 
@@ -59,24 +47,16 @@ interface ConnectionInterface extends \Countable
      * Get a mailbox by its name.
      *
      * @param string $name Mailbox name
-     *
-     * @return MailboxInterface
      */
     public function getMailbox(string $name): MailboxInterface;
 
     /**
      * Create mailbox.
-     *
-     * @param string $name
-     *
-     * @return MailboxInterface
      */
     public function createMailbox(string $name): MailboxInterface;
 
     /**
      * Create mailbox.
-     *
-     * @param MailboxInterface $mailbox
      */
     public function deleteMailbox(MailboxInterface $mailbox): void;
 }
