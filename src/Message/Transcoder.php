26396466c347e84884d8c5063d80afdf0fd97dc9
Filippo Tessarotto <zoeslam@gmail.com>
2021-04-02 16:14:06 +0200

    Require PHP 7.4 for types attributes (#496)
    * src/ ok with PHPStan and PHPUnit
    
    * PHPStan ok on tests/
    
    * CS Fix
    
    * Use baseline for PHPStan for better error tracking
    
    * CI on PHP >= 7.4
    
    * More typed properties
    
    * Prefer inheritDocs

--- src/Message/Transcoder.php
+++ src/Message/Transcoder.php
@@ -9,14 +9,12 @@ use Ddeboer\Imap\Exception\UnsupportedCharsetException;
 final class Transcoder
 {
     /**
-     * @var array
-     *
      * @see https://encoding.spec.whatwg.org/#encodings
      * @see https://dxr.mozilla.org/mozilla-central/source/dom/encoding/labelsencodings.properties
      * @see https://dxr.mozilla.org/mozilla1.9.1/source/intl/uconv/src/charsetalias.properties
      * @see https://msdn.microsoft.com/en-us/library/cc194829.aspx
      */
-    private static $charsetAliases = [
+    private const CHARSET_ALIASES = [
         '128'                       => 'Shift_JIS',
         '129'                       => 'EUC-KR',
         '134'                       => 'GB2312',
@@ -283,8 +281,8 @@ final class Transcoder
 
         $originalFromCharset  = $fromCharset;
         $lowercaseFromCharset = \strtolower($fromCharset);
-        if (isset(self::$charsetAliases[$lowercaseFromCharset])) {
-            $fromCharset = self::$charsetAliases[$lowercaseFromCharset];
+        if (isset(self::CHARSET_ALIASES[$lowercaseFromCharset])) {
+            $fromCharset = self::CHARSET_ALIASES[$lowercaseFromCharset];
         }
 
         \set_error_handler(static function (): bool {
