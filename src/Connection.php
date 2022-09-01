f41a047f78f3311e23dc4e04f2fc5060f6ee92bf
Jakub Bouček <pan@jakubboucek.cz>
2022-09-01 16:37:57 +0200

    Fix: Prevent restore cache on closing connection

--- src/Connection.php
+++ src/Connection.php
@@ -50,9 +50,11 @@ final class Connection implements ConnectionInterface
 
     public function close(int $flag = 0): bool
     {
+        $stream = $this->resource->getStream();
+
         $this->resource->clearLastMailboxUsedCache();
 
-        return \imap_close($this->resource->getStream(), $flag);
+        return \imap_close($stream, $flag);
     }
 
     public function getQuota(string $root = 'INBOX'): array
